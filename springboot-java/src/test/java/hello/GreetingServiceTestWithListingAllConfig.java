c0507809604439c82bc631e99f84c17d2a019972
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-15 15:47:45 +1000

    [springboot-java] Use ContextConfiguration and run as spring test

---
+++ springboot-java/src/test/java/hello/GreetingServiceTestWithListingAllConfig.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, GreetingServiceTestWithManualAppContext.LocalTestConfig.class})
+public class GreetingServiceTestWithListingAllConfig {
+    static class LocalTestConfig {
+        @Bean
+        public GreetingService greetingService(NameFormatter formatter) {
+            return new GreetingService(formatter);
+        }
+    }
+
+    @Autowired
+    GreetingService gs;
+
+    @Test
+    public void greet() {
+        assertEquals("Hello, FORMAT[ryuichi]!", gs.greet("ryuichi"));
+    }
+}
