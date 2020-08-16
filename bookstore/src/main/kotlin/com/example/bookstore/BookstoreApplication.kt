1a9e6c6325cb8f90f531fa636a9c1e38e9e3de0d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-16 23:42:14 +1000

    [bookstore] Mark the lambda as Supplier to match the desired call signature

--- bookstore/src/main/kotlin/com/example/bookstore/BookstoreApplication.kt
+++ bookstore/src/main/kotlin/com/example/bookstore/BookstoreApplication.kt
@@ -4,12 +4,14 @@ import io.undertow.Undertow
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.http.server.reactive.UndertowHttpHandlerAdapter
 import org.springframework.web.reactive.function.BodyInserters.fromObject
-import org.springframework.web.reactive.function.server.*
-import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.server.WebHandler
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder.applicationContext
-import reactor.core.publisher.Mono
+import java.util.function.Supplier
 
 fun main(args: Array<String>) {
     val helloWorldRoute = RouterFunctions.route(
@@ -18,7 +20,7 @@ fun main(args: Array<String>) {
     )
 
     val context = AnnotationConfigApplicationContext().apply {
-        registerBean(WEB_HANDLER_BEAN_NAME, MyWebHandler::class.java, helloWorldRoute)
+        registerBean(WEB_HANDLER_BEAN_NAME, WebHandler::class.java, Supplier { RouterFunctions.toWebHandler(helloWorldRoute) })
         refresh()
     }
 
@@ -28,10 +30,4 @@ fun main(args: Array<String>) {
         .addHttpListener(8080, "0.0.0.0")
         .build()
     server.start()
-}
-
-class MyWebHandler(route: RouterFunction<ServerResponse>) : WebHandler {
-    private val webHandler = RouterFunctions.toWebHandler(route)
-
-    override fun handle(exchange: ServerWebExchange): Mono<Void> = webHandler.handle(exchange)
 }
