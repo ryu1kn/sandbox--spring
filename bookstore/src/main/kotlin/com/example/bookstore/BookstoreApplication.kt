11248f05b353d8a7c03424ecbfbe66caf84ad254
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-16 23:19:40 +1000

    [bookstore] Hand write app starts up

--- bookstore/src/main/kotlin/com/example/bookstore/BookstoreApplication.kt
+++ bookstore/src/main/kotlin/com/example/bookstore/BookstoreApplication.kt
@@ -1,11 +1,34 @@
 package com.example.bookstore
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class BookstoreApplication
+import io.undertow.Undertow
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.http.server.reactive.UndertowHttpHandlerAdapter
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebHandler
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder.applicationContext
+import reactor.core.publisher.Mono
 
 fun main(args: Array<String>) {
-    runApplication<BookstoreApplication>(*args)
+    val helloWorldRoute = RouterFunctions.route(
+            RequestPredicates.path("/hello-world"),
+            HandlerFunction<ServerResponse> { ServerResponse.ok().body(fromObject("Hello World")) }
+    )
+
+    val context = AnnotationConfigApplicationContext()
+    context.registerBean(WEB_HANDLER_BEAN_NAME, MyWebHandler::class.java, helloWorldRoute)
+
+    context.refresh()
+    val handler = applicationContext(context).build()
+    val adapter = UndertowHttpHandlerAdapter(handler)
+    val server = Undertow.builder().addHttpListener(8080, "0.0.0.0").setHandler(adapter).build()
+    server.start()
+}
+
+class MyWebHandler(route: RouterFunction<ServerResponse>) : WebHandler {
+    private val webHandler = RouterFunctions.toWebHandler(route)
+
+    override fun handle(exchange: ServerWebExchange): Mono<Void> = webHandler.handle(exchange)
 }
--- bookstore/src/test/kotlin/com/example/bookstore/BookstoreApplicationTests.kt
+++ bookstore/src/test/kotlin/com/example/bookstore/BookstoreApplicationTests.kt
@@ -1,13 +1,13 @@
 package com.example.bookstore
 
+import org.junit.Assert.assertEquals
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class BookstoreApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `simple test`() {
+        assertEquals(1, 1)
     }
 
 }
