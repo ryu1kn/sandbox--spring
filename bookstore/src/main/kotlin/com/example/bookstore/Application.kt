82ab03c7ec608fc95908036f8c64cb520056383f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-19 00:17:31 +1000

    [bookstore] Use Java Config to define beans and wire them up

--- bookstore/src/main/kotlin/com/example/bookstore/Application.kt
+++ bookstore/src/main/kotlin/com/example/bookstore/Application.kt
@@ -1,12 +1,37 @@
 package com.example.bookstore
 
 import io.undertow.Undertow
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.server.reactive.UndertowHttpHandlerAdapter
-import org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder.applicationContext
+
+@Configuration
+class AppConfig {
+    @Bean(WEB_HANDLER_BEAN_NAME)
+    fun bookstoreWebHandler(router: Router) = RouterFunctions.toWebHandler(router.route)
+
+    @Bean
+    fun router(bookCatalogService: BookCatalogService) = Router(bookCatalogService)
+
+    @Bean
+    fun bookCatalogService(bookRepo: BookRepo, authorRepo: AuthorRepo) =
+        BookCatalogService(bookRepo, authorRepo)
+
+    @Bean
+    fun bookRepo() = RealBookRepo()
+
+    @Bean
+    fun authorRepo() = RealAuthorRepo()
+}
 
 fun main() {
-    val bookCatalog = BookCatalogService(RealBookRepo(), RealAuthorRepo())
-    val handler = toHttpHandler(Router(bookCatalog).route)
+    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
+    val handler = applicationContext(context).build()
+
     undertowServer {
         setHandler(UndertowHttpHandlerAdapter(handler))
         addHttpListener(8080, "0.0.0.0")
--- bookstore/src/test/kotlin/com/example/bookstore/ApplicationTests.kt
+++ bookstore/src/test/kotlin/com/example/bookstore/ApplicationTests.kt
@@ -1,16 +1,31 @@
 package com.example.bookstore
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 
 typealias BookInfo = Map<String, String>
 
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [AppConfig::class, ApplicationTests.TestConfig::class])
 class ApplicationTests {
-    private val bookRepo = BookRepo { listOf(RawBook("Book Z", "author-z")) }
-    private val authorRepo = AuthorRepo { id -> if (id == "author-z") "Author Z" else null }
-    private val bookCatalog = BookCatalogService(bookRepo, authorRepo)
-    private val router = Router(bookCatalog)
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun bookRepo() = BookRepo { listOf(RawBook("Book Z", "author-z")) }
+
+        @Bean
+        fun authorRepo() = AuthorRepo { id -> if (id == "author-z") "Author Z" else null }
+    }
+
+    @Autowired
+    lateinit var router: Router
 
     @Test
     fun `Retrieve all books`() {
